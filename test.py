a4c1dc05f2624d66136224d37080b78ca53d1233
Senh Mo Chuang <senhmo@gmail.com>
2021-08-19 02:54:54 +0100

    first commit

---
+++ test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from GPIORPi import GPIORPi
+
+seconds = .15
+with GPIORPi(mode=False) as rpi:
+    for each_io in [14, 15, 17, 18]:
+        rpi.output_latch_down(each_io, seconds)
+    
+    
+    
+

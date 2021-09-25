d6075f786e18263f0c801ce04af295a2a5bce787
Senh Mo Chuang <senhmo@gmail.com>
2021-09-24 22:43:08 -0500

    refactor

--- GPIORPi/__init__.py
+++ GPIORPi/__init__.py
@@ -2,60 +2,28 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 
-class GPIORPi:
-    def __init__(self, mode=False) -> None:
+class MyRPi:
+    def __init__(self, board_mode=GPIO.BCM, input_pins=[], output_pins=[]) -> None:
         self.GPIO = GPIO
-        if mode:
-            self.GPIO.setmode(GPIO.BOARD)
-        else:
-            self.GPIO.setmode(GPIO.BCM)
+        self.setmode(board_mode)
+        map(lambda pin: self.GPIO.setup(int(pin), self.GPIO.IN), input_pins)
+        map(lambda pin: self.GPIO.setup(int(pin), self.GPIO.OUT), output_pins)
 
     def output_latch_down(self, pin, seconds):
-        self.GPIO.setup(pin, self.GPIO.OUT)
         self.GPIO.output(pin, self.GPIO.LOW)
         sleep(seconds)
         self.GPIO.output(pin, self.GPIO.HIGH)
 
     def output_latch_up(self, pin, seconds):
-        self.GPIO.setup(pin, self.GPIO.OUT)
         self.GPIO.output(pin, self.GPIO.HIGH)
         sleep(seconds)
         self.GPIO.output(pin, self.GPIO.LOW)
 
     def read_pin(self, pin):
-        self.GPIO.setup(pin, self.GPIO.IN)
-        return GPIO.input(pin)
+        return self.GPIO.input(pin)
 
     def __enter__(self):
         return self
 
     def __exit__(self, exec_type, exec_value, traceback):
         self.GPIO.cleanup()
-
-
-class MyGPIO(GPIO):
-    def __init__(self, board_mode=GPIO.BCM, input_pins=[], output_pins=[]) -> None:
-        super().__init__()
-        self.setmode(board_mode)
-        map(lambda pin: self.setup(int(pin), self.IN), input_pins)
-        map(lambda pin: self.setup(int(pin), self.OUT), output_pins)
-
-    def active_high_after_x_sec(self, pin, sec):
-        self.output(pin, self.LOW)
-        sleep(sec)
-        self.output(pin, self.HIGH)
-
-    def active_low_after_x_sec(self, pin, sec):
-        self.output(pin, self.HIGH)
-        sleep(sec)
-        self.output(pin, self.LOW)
-
-    def read_pin(self, pin):
-        self.setup(pin, self.IN)
-        return self.input(pin)
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exec_type, exec_value, traceback):
-        self.cleanup()
--- test.py
+++ test.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 from GPIORPi import GPIORPi
 
-seconds = .15
-with GPIORPi(mode=False) as rpi:
+seconds = 0.15
+with MyRPi(input_pins=[14, 15, 17, 18]) as rpi:
     for each_io in [14, 15, 17, 18]:
         rpi.output_latch_down(each_io, seconds)
-    
-    
-    
-

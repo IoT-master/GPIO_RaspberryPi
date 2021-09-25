28f3aa81d8d72f46e0db1a8b8d7a2ec96dce368b
Senh Mo Chuang <senhmo@gmail.com>
2021-09-24 23:10:22 -0500

    code cleanup:

--- GPIORPi/__init__.py
+++ GPIORPi/__init__.py
@@ -3,27 +3,26 @@ from time import sleep
 
 
 class MyGPIO:
-    def __init__(self, board_mode=GPIO.BCM, input_pins=[], output_pins=[]) -> None:
-        self.GPIO = GPIO
-        self.GPIO.setmode(board_mode)
-        list(map(lambda pin: self.GPIO.setup(int(pin), self.GPIO.IN), input_pins))
-        list(map(lambda pin: self.GPIO.setup(int(pin), self.GPIO.OUT), output_pins))
+    def __init__(self, board_mode=GPIO.BCM, input_pins=[], output_pins=[]):
+        GPIO.setmode(board_mode)
+        list(map(lambda pin: GPIO.setup(int(pin), GPIO.IN), input_pins))
+        list(map(lambda pin: GPIO.setup(int(pin), GPIO.OUT), output_pins))
 
     def output_latch_down(self, pin, seconds):
-        self.GPIO.output(pin, self.GPIO.LOW)
+        GPIO.output(pin, GPIO.LOW)
         sleep(seconds)
-        self.GPIO.output(pin, self.GPIO.HIGH)
+        GPIO.output(pin, GPIO.HIGH)
 
     def output_latch_up(self, pin, seconds):
-        self.GPIO.output(pin, self.GPIO.HIGH)
+        GPIO.output(pin, GPIO.HIGH)
         sleep(seconds)
-        self.GPIO.output(pin, self.GPIO.LOW)
+        GPIO.output(pin, GPIO.LOW)
 
     def read_pin(self, pin):
-        return self.GPIO.input(pin)
+        return GPIO.input(pin)
 
     def __enter__(self):
         return self
 
     def __exit__(self, exec_type, exec_value, traceback):
-        self.GPIO.cleanup()
+        GPIO.cleanup()
